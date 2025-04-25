926e6bb563bf1131146f27f898107aa935b5eecc
kukio <fan2487373152@gmail.com>
2025-04-25 21:18:37 +0800

    modified:   components/display2D/index.jsx 	modified:   components/displayPart/display.module.css 	modified:   index.html 	modified:   vite.config.js

--- components/display2D/index.jsx
+++ components/display2D/index.jsx
@@ -1,10 +1,10 @@
 /*
  * @Author: tErrAn 2487373152@qq.com
  * @Date: 2025-04-18 19:57:05
- * @LastEditors: tErrAn 2487373152@qq.com
- * @LastEditTime: 2025-04-18 21:26:58
+ * @LastEditors: kukio fan2487373152@gmail.com
+ * @LastEditTime: 2025-04-25 21:03:28
  * @FilePath: \close_fitness_2D\components\display2D\index.jsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 
  */
 
 // import React, { useState, useCallback } from 'react';
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: kukio fan2487373152@gmail.com
+ * @Date: 2025-04-25 20:56:38
+ * @LastEditors: kukio fan2487373152@gmail.com
+ * @LastEditTime: 2025-04-25 21:06:46
+ * @FilePath: /visual-close-fiting/vite.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://1f1a-58-248-162-168.ngrok-free.app',
+        target: 'https://6aee-183-236-19-153.ngrok-free.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
